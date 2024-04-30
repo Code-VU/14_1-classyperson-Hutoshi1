3be5a54cc29d512833af19146fce4693c713ca18
Tony Davis <Tony.Davis@vu.com>
2024-04-30 15:00:04 -0500

    added class

--- classy_person.py
+++ classy_person.py
@@ -8,6 +8,23 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name 
+
+    def increase_age(self):
+        self.age += 1
+        print(self.age)
+
+    def say_greeting(self):
+        print(f"Hello world! My name is {self.name}!")
+
+    def count_to_age(self):
+        count = 0
+        while count <= self.age:
+            count += 1
+            print(count)
+
 
 
 
